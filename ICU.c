54868550f99a50c4ee4688c4433dbbfaab2b8182
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-18 16:50:06 +0200

    first commit

---
+++ ICU.c
@@ -0,0 +1,17 @@
+﻿/*
+ * ICU.c
+ *
+ * Created: 18/02/2020 03:18:01 م
+ *  Author: mo
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ ICU_Config.h
@@ -0,0 +1,16 @@
+﻿/*
+ * ICU_Config.h
+ *
+ * Created: 18/02/2020 03:19:39 م
+ *  Author: mo
+ */ 
+
+
+#ifndef ICU_CONFIG_H_
+#define ICU_CONFIG_H_
+
+
+
+
+
+#endif /* ICU_CONFIG_H_ */
